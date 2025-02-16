8efcebbc657a546fcdaaafcbf8c329be7979511d
GauravNandgowli <me.nandgowligaurav@gmail.com>
2025-02-16 14:02:38 +0000

    added recursive program for best case divisor program

--- recursion/recur_bestDivisors.java
+++ recursion/recur_bestDivisors.java
@@ -6,12 +6,30 @@ public class recur_bestDivisors {
 
     public static void main(String[] args) {
         ArrayList<Integer> arr = new ArrayList<>();
-        System.out.println(recur(36, 1, arr));
-        ;
+        System.out.println(forwardRecur(36, 1, arr));
+        System.out.println(backwardRecur(36, 1));
+
     }
 
-    static ArrayList<Integer> recur(int n, int i, ArrayList<Integer> arr) {
+    static ArrayList<Integer> forwardRecur(int n, int i, ArrayList<Integer> arr) {
 
+        if (i > (Math.sqrt(n))) {
+            System.out.println("last array  " + arr);
+            return arr;
+        }
+        if (n % i == 0) {
+            arr.add(i);
+            int val = n / i;
+            arr.add(val);
+        }
+        i++;
+        forwardRecur(n, i, arr);
+        return arr;
+
+    }
+
+    static ArrayList<Integer> backwardRecur(int n, int i) {
+        ArrayList<Integer> arr = new ArrayList<>();
         if (i > (Math.sqrt(n))) {
             return arr;
         }
@@ -21,8 +39,8 @@ public class recur_bestDivisors {
             arr.add(val);
         }
         i++;
-        recur(n, i, arr);
+        ArrayList<Integer> fin = backwardRecur(n, i);
+        arr.addAll(fin);
         return arr;
-
     }
 }
